30ff011e13e142c34deae577c76d2bb07edb9c25
muhammadalimahmudov <mohammadmahmudoff@gmail.com>
2025-12-19 19:46:30 +0500

    Enhance home page carousel with localized content and auto-play functionality

--- lib/presentation/pages/home/presentation/home_page.dart
+++ lib/presentation/pages/home/presentation/home_page.dart
@@ -1,7 +1,10 @@
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:thousand_melochey/core/imports/imports.dart';
+import 'package:thousand_melochey/presentation/pages/home/presentation/widgets/carousel_slide_widget.dart';
 import 'package:thousand_melochey/presentation/pages/home/presentation/widgets/categories/all_products_widget.dart';
+
+import '../../../../service/localizations/localization.dart';
 
 @RoutePage()
 class HomePage extends ConsumerStatefulWidget {
@@ -62,23 +65,44 @@ class _HomePageState extends ConsumerState<HomePage>
                     slivers: [
                       SliverToBoxAdapter(
                           child: CarouselSlider(
-                        options: CarouselOptions(height: 150.h, viewportFraction: 0.9),
-                            items: [1, 2, 3, 4].map((i) {
-                              return Builder(
-                                builder: (BuildContext context) {
-                                  return Container(
-                                      width: MediaQuery.of(context).size.width,
-                                      margin: const EdgeInsets.symmetric(horizontal: 5.0, vertical: 4),
-                                      decoration: BoxDecoration(
-                                          color: Colors.amber,
-                                        borderRadius: BorderRadius.circular(12.r)
-                                      ),
-                                      child: Text('text $i', style: const TextStyle(fontSize: 16.0),)
-                                  );
-                                },
-                              );
-                            }).toList(),
-                      )),
+                  options: CarouselOptions(
+                    height: 150.h,
+                    viewportFraction: 0.9,
+                    autoPlay: true,
+                    autoPlayInterval: const Duration(seconds: 4),
+                    autoPlayAnimationDuration: const Duration(milliseconds: 800),
+                    autoPlayCurve: Curves.fastOutSlowIn,
+                    enlargeCenterPage: true,
+                    enlargeFactor: 0.2,
+                  ),
+                  items: [
+                    CarouselSlideWidget(
+                      title: AppLocalization.getText(context)?.carousel_delivery ?? '',
+                      icon: Icons.local_shipping_rounded,
+                      accentColor: AppColors.primaryColor,
+                      slideIndex: 0,
+                    ),
+                    CarouselSlideWidget(
+                      title: AppLocalization.getText(context)?.carousel_popular ?? '',
+                      icon: Icons.star_rounded,
+                      accentColor: Color(0xFFF57C00),
+                      slideIndex: 1,
+                    ),
+                    CarouselSlideWidget(
+                      title: AppLocalization.getText(context)?.carousel_search ?? '',
+                      icon: Icons.search_rounded,
+                      accentColor: AppColors.primaryShadeColor,
+                      slideIndex: 2,
+                    ),
+                    CarouselSlideWidget(
+                      title: AppLocalization.getText(context)?.carousel_payment ?? '',
+                      icon: Icons.payment_rounded,
+                      accentColor: AppColors.red,
+                      slideIndex: 3,
+                    ),
+                  ],
+                ),
+                      ),
                       const SliverToBoxAdapter(child: SizedBox(height: 10,),),
                       const ProductsListWidget()
                     ],
